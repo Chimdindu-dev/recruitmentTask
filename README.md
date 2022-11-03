3488c22d6aad5cec5061c96804d0be53ba748b18
Chimdindu-dev <cchimdindu@gmail.com>
2022-11-03 17:07:11 +0200

    changed the csv file for testing

--- README.md
+++ README.md
@@ -5,17 +5,19 @@
 calculates the <b>.json</b> files sha256 and appeneds to a new <b>.csv</b> file</p>
 <p>Takes one <b>.csv</b> as the input</p>
 
+<p><b>Code was written in Ubuntu22.04</b></p>
+
 <h3>How to use</h3>
 <ol>
 <li>Install python3</li>
-<li>Download HNG9Folder/or Download project Zip</li>
-<li>Move your .csv file into the HNG9Folder</li>
-<li>Open your terminal and navigate to HNG9Folder on your system</li>
-<li>There is a <b>TeamRuler.csv</b> you can test your code with in the HNG9Folder</li>
+<li>Download Download project Zip and unzip it</li>
+<li>Move your .csv file into the Project folder</li>
+<li>Open your terminal and navigate to the Folder on your system</li>
+<li>There is a <b>HNGi9CSV.csv</b> you can test your code with in the Folder</li>
 <li>In your terminal type: <b>python3 csvtojson.py</b></li>
 <li>You will be prompted to enter the csv filename</li>
 <ul>
-<li>There is a <b>TeamRuler.csv</b> you can test your code with in the HNG9Folder</li>
+<li>There is a <b>HNGi9CSV.csv</b> you can test your code with in the Folder</li>
 <li>DO NOT ENTER THE .csv only enter file name</li>
 <li>You must change Filenames with spaces eg. NFT team 50 Team Ruler.csv -> TeamRuler.csv</li>
 <li>Typing .csv at the end of your file will lead to the file not being discovered</li>
---
+++ csvtojson.py
@@ -0,0 +1,90 @@
+import csv
+import json
+import hashlib
+from pathlib import Path
+import os
+
+#creating json files from csv file provided in argument
+#open the csv file
+inFile = input('Enter a file name [do not include file type]: ')
+csvfile = open(inFile+'.csv','r')
+csvreader = csv.DictReader(csvfile)
+
+nftname = []
+seriesno = []
+description = []
+gender = []
+id = []
+anothername = []
+attributes = []
+count = 0
+for row in csvreader:
+    nftname.append(row['Filename'])
+    seriesno.append(row['Series Number'])
+    description.append(row['Description'])
+    gender.append(row['Gender'])
+    id.append(row['UUID'])
+    anothername.append(row['Name'])
+    attributes.append(row['Attributes'])
+
+    if seriesno[count].isnumeric() == True:
+        up = int(seriesno[count])
+    else:
+        up = seriesno[count]
+
+    json_object = {
+                'format' : 'CHIP-0007',
+                'name' : nftname[count],
+                'description' : description[count],
+                'sensitive_content' : False,
+                'series_number' : up,
+                'series_total' : 400,
+                'attributes' :[ {
+                    'trait_type' : 'Gender',
+                    'value' : gender[count]
+                }
+                ],
+                'collection' : {
+                    'name' : 'HNG9Zuri NFT Collection',
+                    'id' : id[count],
+                    "attributes": [{
+                "type": "description",
+                "value": "NFT Collection for zuri internship"}]
+                },
+    }  
+    out = json.dumps(json_object, indent=4)
+    jsonoutput = open(str(nftname[count])+'.json','w')
+    jsonoutput.write(out)
+    count +=1
+
+jsonoutput.close()
+
+#hash calulator - calulating sha256 of each json file and out a csv file
+input_directory = Path.cwd()
+# glob = specific folder
+# rglob = including sub floders
+files = list(input_directory.glob("*.json"))
+list = list()
+
+for path in files:
+    with open(path,'rb') as opened_file:
+        content = opened_file.read()
+        sha256 = hashlib.sha256()
+        sha256.update(content)
+        sha = sha256.hexdigest()
+        list.append(sha)
+        list[0] = "Sha256"
+        i = 0
+
+        with open(inFile +'.csv','r') as read_obj,\
+            open(inFile +'.output.csv','w',newline='') as write_obj:
+            csv_reader = csv.reader(read_obj)
+            csv_writer = csv.writer(write_obj)
+            for row in csv_reader:
+                if i>len(list)-1:
+                    break
+                row.append(list[i])
+                csv_writer.writerow(row)
+                i+=1
+
+csvfile.close()
